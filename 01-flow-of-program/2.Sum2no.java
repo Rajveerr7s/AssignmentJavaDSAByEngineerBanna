fd2594499c4545db59cf5c05755dd08671e68db3
Rajveer Singh <33891278+Rajveerr7s@users.noreply.github.com>
2021-11-07 23:24:57 +0530

    Sum 2 no program

---
+++ 01-flow-of-program/2.Sum2no.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Sum {
+    public static void main(String args[]){
+    int num1,num2 ;
+    Scanner in= new Scanner(System.in);
+    System.out.println("Enter num1");
+    num1=in.nextInt();
+    System.out.println("Enter num2");
+    num2=in.nextInt();
+
+    System.out.println(num1+num2);
+    }
+}
